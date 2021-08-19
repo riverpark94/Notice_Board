bf7eba7e5bd1952d8a2a43fa202c5cbed136f8cd
riverpark94 <zhel7077@naver.com>
2021-08-19 15:33:41 +0900

    Edit1 : #2 join(CheckPassword.js에서 변수명을 Camel-case로 변경)

--- middlewares/CheckPassword.js
+++ middlewares/CheckPassword.js
@@ -1,9 +1,9 @@
 const checkPassword = (paw) => {
-  const paw_num = paw.search(/[0-9]/);
-  const paw_eng = paw.search(/[a-zA-Z]/);
+  const pawNum = paw.search(/[0-9]/);
+  const pawEng = paw.search(/[a-zA-Z]/);
   // const paw_ENG = paw.search(/[A-Z]/);
 
-  if (paw_num < 0 || paw_eng < 0) {
+  if (pawNum < 0 || pawEng < 0) {
     return false;
   }
   else {
