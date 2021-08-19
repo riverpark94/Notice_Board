883f3df75f8ea566e9e4ef8be685681f4ebd45ec
riverpark94 <zhel7077@naver.com>
2021-08-19 14:59:40 +0900

    Doing : #2 join (이메일, 닉네임, 비밀번호 칼럼이 비어있는지 확인)

--- controllers/Users/join.js
+++ controllers/Users/join.js
@@ -4,5 +4,10 @@ const Users = require('../../models');
 module.exports = {
   post : (req, res) =>{
     console.log(req.body);
+    const { email, nickname, password } = req.body;
+
+    if( !email || !nickname  || !password) {
+      res.status(400).send("not enough user information.");
+    }
   }
 }
