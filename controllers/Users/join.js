cf19766560db66e1c87bd512237aae58f2203c98
riverpark94 <zhel7077@naver.com>
2021-08-19 15:02:56 +0900

    Doing : #2 join(닉네네임 길이 확인)

--- controllers/Users/join.js
+++ controllers/Users/join.js
@@ -5,9 +5,13 @@ module.exports = {
   post : (req, res) =>{
     console.log(req.body);
     const { email, nickname, password } = req.body;
+    // console.log("[nickname 길이] : ", nickname.length)
 
     if( !email || !nickname  || !password) {
       res.status(400).send("not enough user information.");
+    }
+    else if (nickname.length > 10) {
+      res.status(403).send("make nickname less than 10 characters.");
     }
   }
 }
