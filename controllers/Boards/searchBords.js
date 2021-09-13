160606e8e6936e56c7a14eec04a939552f7b38a5
riverpark94 <zhel7077@naver.com>
2021-09-13 15:10:09 +0900

    Complete : #10 searchBords

--- controllers/Boards/searchBords.js
+++ controllers/Boards/searchBords.js
@@ -1,6 +1,18 @@
 const { Boards } = require('../../models');
+const { Op } = require("sequelize");
 
 module.exports = {
   get : async (req, res) =>{
+    const { search } = req.body;
+
+    Boards.findAll({
+      where : {
+        title : {
+                [Op.like]: "%" + search + "%"
+            }
+      }
+    }).then(data => {
+        res.status(200).send(data)
+    })
   }
 }
